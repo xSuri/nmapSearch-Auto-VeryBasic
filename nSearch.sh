12ad31bb192adf9df0da9f3660a4c6acd748c687
suri <54814232+suri092004@users.noreply.github.com>
2020-07-25 19:32:51 +0200

    Add files via upload

---
+++ nSearch.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "Starting... \n"
+
+echo "Ip? \n"
+read IPs
+
+echo "Ip ""$IPs"" is correct? (y/n)"
+read correct
+
+yes="y"
+no="n"
+
+if [ "$correct" = "$yes" ];
+    then
+
+    touch log.txt
+    chmod 755 log.txt
+
+    sudo nmap -sV -sC -sS -vv "$IPs" >> log.txt
+    
+
+
+fi
+
+if [ "$correct" = "$no" ];
+    then
+
+    echo "Program stoped... (Start again and write correct IP)"
+
+fi
+
+echo "Program stoped... Your result in file log.txt"
---
+++ removeLogs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Removing Logs... \n"
+
+rm -R log.txt
+
+echo "\n Logs removed"
